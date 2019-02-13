ae28cce37d77aa8238fe56362a9f1fc1264568cd
Adam Kelso <kelso.adam@gmail.com>
2019-02-12 23:16:44 -0600

    Cleaning up the Expectations class. Everything should be a filter, as we're being explicit with expectations. Assertions should be done on the wrapper class.

--- src/Expectation.php
+++ src/Expectation.php
@@ -2,8 +2,6 @@
 
 namespace Guzzler;
 
-use GuzzleHttp\Psr7\Request;
-use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\MockObject\Invocation\ObjectInvocation;
 use PHPUnit\Framework\MockObject\Matcher;
 use PHPUnit\Framework\TestCase;
@@ -29,10 +27,19 @@ class Expectation
 
     protected $withs = [];
 
+    protected $filters = [];
+
     public function __construct(Matcher\InvokedRecorder $times, Wrapper $wrapper)
     {
         $this->times = $times;
         $this->wrapper = $wrapper;
+    }
+
+    protected function addFilter($filter)
+    {
+        if (!in_array($filter, $this->filters)) {
+            $this->filters[] = $filter;
+        }
     }
 
     public function endpoint(string $uri, string $method)
@@ -40,12 +47,16 @@ class Expectation
         $this->endpoint = $uri;
         $this->method = $method;
 
+        $this->addFilter('endpoint');
+
         return $this;
     }
 
     public function withHeader(string $key, $value = null)
     {
         $this->headers[$key] = $value;
+
+        $this->addFilter('headers');
 
         return $this;
     }
@@ -62,6 +73,8 @@ class Expectation
     public function withBody(string $body)
     {
         $this->body = $body;
+
+        $this->addFilter('body');
 
         return $this;
     }
@@ -103,40 +116,49 @@ class Expectation
         return $this;
     }
 
+    protected function filterByEndpoint(array $history)
+    {
+        return array_filter($history, function($call) {
+            return $call['request']->getMethod() == $this->method
+                && $call['request']->getUri() == $this->endpoint;
+        });
+    }
+
+    protected function filterByHeaders(array $history)
+    {
+        return array_filter($history, function($call) {
+            foreach ($this->headers as $key => $value) {
+                $header = $call['request']->getHeader($key);
+
+                if ($header != $value && !in_array($value, $header)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+    }
+
+    protected function filterByBody(array $history) {
+        return array_filter($history, function($call) {
+            return $call['request']->getBody() == $this->body;
+        });
+    }
+
     /**
-     * Iterate over the history and run assertions against it.
+     * Iterate over the history and verify the invocations against it.
      *
      * @param TestCase $instance
      * @param array $history
      */
     public function __invoke(TestCase $instance, array $history): void
     {
-        $invocations = array_filter($history, function($call) {
-            return $call['request']->getMethod() == $this->method
-                && $call['request']->getUri() == $this->endpoint;
-        });
+        foreach ($this->filters as $filter) {
+            $history = $this->{'filterBy'.ucfirst($filter)}($history);
+        }
 
-        foreach ($invocations as $i) {
-            $this->times->invoked(new ObjectInvocation('','',[],'',$i));
-
-            /** @var Request $request */
-            $request = $i['request'];
-
-            // Headers
-            foreach ($this->headers as $key => $value) {
-                $message = "Failed asserting that the header {$key} is equal to {$value}.";
-
-                if (is_array($value)) {
-                    Assert::assertEquals($value, $request->getHeader($key), $message);
-                    continue;
-                }
-
-                Assert::assertContains($value, $request->getHeader($key), $message);
-            }
-
-            if (!empty($this->body)) {
-                Assert::assertEquals($this->body, $request->getBody());
-            }
+        foreach ($history as $i) {
+            $this->times->invoked(new ObjectInvocation('','',[],'',$i['request']));
         }
 
         // Invocation Counts
